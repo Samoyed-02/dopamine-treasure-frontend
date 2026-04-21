3d10ae6821885409e758442395fa10b4b7157358
Samoyed-02 <gus02031365@gmail.com>
2026-04-22 02:45:02 +0900

    수정

--- dopamine-treasure-island/vite.config.js
+++ dopamine-treasure-island/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'  // ← path 대신 이걸로
 
 export default defineConfig({
-  base: 'dopamine-treasure-frontend/',
+  base: '/dopamine-treasure-frontend/',
   plugins: [react()],
   resolve: {
     alias: {
